0ec400dd0af0b5b6043fd9e29d16c038816c46ed
Ramon Medeiros <ramon@juni.co>
2024-12-20 11:27:33 +0300

    working with guest list

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import './App.css';
+import Guests from './guests';
 
 const App = () => {
-  return (
 
-      <div className="card">
-       
-        <h1>Save the date</h1>
-        <div className="subtitle">Wedding reception</div>
-        <div className="date">07 | JUNE | 2025</div>
-        
-        <div className="arch-illustration"></div>
-        
-        <div className="couple">Sintuya & Haresh</div>
-        <div className="footer">Official invitation to follow</div>
-      </div>
+
+  const getGuestName = () => {
+    if (window.location.pathname === "/") {
+      return ""
+    }
+
+
+    for (let i = 0; i < Guests.length; i++) {
+      if (window.location.pathname.includes(Guests[i].url)) {
+        return `${Guests[i].person1} and ${Guests[i].person2}`
+      }
+    }
+
+    return ""
+  }
+
+  return (
+    <div className="card">
+
+      <h1>Save the date</h1>
+      <h2>{getGuestName()}</h2>
+
+      <div className="subtitle">Wedding reception</div>
+      <div className="date">07 | JUNE | 2025</div>
+
+      <div className="arch-illustration"></div>
+
+      <div className="couple">Sintuya & Haresh</div>
+      <div className="footer">Official invitation to follow</div>
+    </div>
   );
 };
 
---
+++ src/guests.tsx
@@ -0,0 +1,21 @@
+
+interface Guest {
+    person1: string;
+    person2: string;
+    url: string;
+}
+
+const Guests: Guest[] = [
+    {
+        "person1": "John Smith",
+        "person2": "Jane Doe",
+        "url": "john_jane_smith"
+    },
+    {
+        "person1": "Astrid Johansson",
+        "person2": "William Johansson",
+        "url": "astrid_william_johansson"
+    }
+]
+
+export default Guests;
