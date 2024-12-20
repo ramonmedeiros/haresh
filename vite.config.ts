cf4e339079a24d74d3e278b1f61ca3a7a4186f94
Ramon Medeiros <ramon@juni.co>
2024-12-20 11:46:53 +0300

    fix background size and assets link

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://haresh.ramonmedeiros.dev"
+  base: "https://haresh.ramonmedeiros.dev/"
 })
